3829b34693edb7b7d8afd172347f0ed3867681c0
sergeyshoote <rusinovitch.sergey@gmail.com>
2022-03-25 18:32:04 +0300

    list of cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  build(context) {
+class _MyAppState extends State<MyApp> {
+  final List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList'),
+          title: const Text('EasyList'),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Add Product'),
+              onPressed: () {
+                setState(() {
+                  _products.add('Advance Food Tester');
+                });
+              },
+            ),
+            Column(
+              children: _products
+                  .map(
+                    (element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/food.jpeg'),
+                          Text(element)
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ],
         ),
       ),
     );
